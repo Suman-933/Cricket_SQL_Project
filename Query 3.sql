5d669813bece9fd8510589b231dd6b9a2421a9e8
Suman Das <dassuman6768@gmail.com>
2025-10-01 19:25:21 +0530

    Delete Query 3.sql

--- Query 3.sql
+++
@@ -1,9 +0,0 @@
- -- Players with 150+ strike rate
- select all_players.Player_Name, ((Strike.Runs_scored/Strike.Balls_faced)*100)as Strike_rate
-from all_players
-join (select Player_Id,Runs_Scored,Balls_faced from 1st_innings_batting
-union all
-select Player_Id,Runs_Scored,Balls_faced from 2nd_innings_batting) as Strike
-on all_players.Player_Id= Strike.Player_Id
-where ((Strike.Runs_scored/Strike.Balls_faced)*100) >= 150
-order by strike_rate desc
